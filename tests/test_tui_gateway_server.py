c123cf10839cedaa5bc82d057a3c27a08e74c449
Tony Semana <tony.semana@gmail.com>
2026-05-20 11:59:12 -0400

    test(tui): accept macOS browser launch hint
    (cherry picked from commit 683113e57750650d859589df8fa28c698dfd5c79)
    (cherry picked from commit bf37acbc9d02490137d38435236e013eda66568a)

--- tests/test_tui_gateway_server.py
+++ tests/test_tui_gateway_server.py
@@ -6626,6 +6626,7 @@ def test_browser_manage_connect_default_local_reports_launch_hint(monkeypatch):
     )
     assert any(
         "No supported Chromium-family browser executable was found" in line
+        or "Start Chrome with remote debugging" in line
         for line in resp["result"]["messages"]
     )
     assert any(
